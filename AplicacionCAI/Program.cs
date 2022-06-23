4c60fbc3d7ba9c9bc881aa6846b23295d7c8b902
JoelCAI <102713088+JoelCAI@users.noreply.github.com>
2022-06-22 22:51:16 -0300

    Update Program.cs
    Solucion al bucle de inicio

--- AplicacionCAI/Program.cs
+++ AplicacionCAI/Program.cs
@@ -12,16 +12,15 @@ namespace AplicacionCAI
 
         static void Main(string[] args)
         {
-           
-           Menu();
-           
-         
 
+             MenuPrincipal();
+             Validador.Despedida();
+ 
         }
 
-        public static void Menu()
+        public static void MenuPrincipal()
         {
-            
+
             int ingreso;
             int dni = 0;
             string nombre = "";
@@ -31,100 +30,87 @@ namespace AplicacionCAI
             do
             {
                 Console.Clear();
-                
+
                 ingreso = Validador.PedirIntMenu("\n Aplicación Corporativa." +
                                        "\n [1] Ingresar Como Usuario Corporativo. " +
                                        "\n [2] Salir del Sistema.", 1, 2);
 
-                switch (ingreso)
+                if (ingreso == 1)
                 {
-                    case 1:
+                    Console.Clear();
+                    var usuarioDni = DiccionarioUsuario.BuscarUsuarioDni();
+
+                    if (usuarioDni != null && ingreso != 3)
+                    {
                         Console.Clear();
-                        var usuarioDni = DiccionarioUsuario.BuscarUsuarioDni();
+                        Console.WriteLine("\n Ingrese su Clave");
+                        string validacion = Console.ReadLine().ToUpper();
 
-
-
-                        if (usuarioDni != null)
+                        if (validacion == usuarioDni.ClaveUsuario)
                         {
-                            var usuarioClave = DiccionarioUsuario.BuscarUsuarioClave();
+                            dni = usuarioDni.DniUsuario;
+                            nombre = usuarioDni.NombreUsuario;
+                            clave = usuarioDni.ClaveUsuario;
+                            cuit = usuarioDni.CuitCorporativo;
 
-                            if (usuarioClave != null)
+                            int opcion;
+                            do
                             {
-                                dni = usuarioDni.DniUsuario;
-                                nombre = usuarioDni.NombreUsuario;
-                                clave = usuarioDni.ClaveUsuario;
-                                cuit = usuarioDni.CuitCorporativo;
+                                Console.Clear();
+                                Console.WriteLine("\n Bienvenid@ " + nombre + "\n Su Cuit es: " + cuit.ToString());
+                                opcion = Validador.PedirIntMenu("\n Menú del Usuario Corporativo" +
+                                                       "\n [1] Solicitar un Pedido de correspondencia o Encomienda. " +
+                                                       "\n [2] Consultar el Estado de un Pedido. " +
+                                                       "\n [3] Consultar Estado de Cuenta. " +
+                                                       "\n [4] Salir del Sistema.", 1, 4);
 
-                                int opcion;
-                                do
+                                switch (opcion)
                                 {
-                                    Console.Clear();
-                                    Console.WriteLine("\n Bienvenid@ " + nombre + "\n Su Cuit es: " + cuit.ToString());
-                                    opcion = Validador.PedirIntMenu("\n Menú del Usuario Corporativo" +
-                                                           "\n [1] Solicitar un Pedido de correspondencia o Encomienda. " +
-                                                           "\n [2] Consultar el Estado de un Pedido. " +
-                                                           "\n [3] Consultar Estado de Cuenta. " +
-                                                           "\n [4] Salir del Sistema.", 1, 4);
+                                    case 1:
+                                        Console.Clear();
+                                        GenerarSolicitudPedido();
+                                        break;
+                                    case 2:
+                                        Console.Clear();
+                                        ConsultarEstadoPedido();
+                                        break;
+                                    case 3:
+                                        Console.Clear();
+                                        ConsultarCuenta(cuit);
+                                        break;
 
-                                    switch (opcion)
-                                    {
-                                        case 1:
-                                            Console.Clear();
-                                            GenerarSolicitudPedido();
-                                            break;
-                                        case 2:
-                                            Console.Clear();
-                                            ConsultarEstadoPedido();
-                                            break;
-                                        case 3:
-                                            Console.Clear();
-                                            ConsultarCuenta(cuit);
-                                            break;
+                                }
+                            } while (opcion != 4);
 
-                                    }
-                                } while (opcion != 4);
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("Digitó una clave incorrecta, vuelvalo a intentar con los datos correctos");
-                                Validador.VolverMenu();
-                            }
-
-
-                        }
+                        } 
                         else
                         {
-                            Console.WriteLine("No existe el usuario, intente con otro usuario");
-                            usuarioDni = null;
-                            Validador.VolverMenu();
+                            Console.Clear();
+                            Console.WriteLine("\n Ingresó una clave incorrecta, presione cualquier tecla para volver al Menú Principal");
+                            Console.ReadKey();
                         }
                         
-                        break;
-                    case 2:
-                        
-                    break;
+
+                    }
+                    
 
                 }
+
             } while (ingreso != 2);
 
         }
-        
-        public static void Continuar()
-        {
 
-        }
 
         private static void GenerarSolicitudPedido()
         {
             var pedido = Pedido.CrearPedido();
             Console.WriteLine($"El costo total por el servicio es: {pedido.TotalCalculoPedido}");
-            
+
             do
             {
                 Console.WriteLine("¿Desea confirmar? Responder S/N");
                 string ingreso = Console.ReadLine();
-                string opcion = ingreso.ToUpper();          
+                string opcion = ingreso.ToUpper();
 
                 if (opcion == "S")
                 {
@@ -132,34 +118,37 @@ namespace AplicacionCAI
                     Console.WriteLine("Su pedido se generó correctamente.");
                     break;
                 }
+
                 if (opcion == "N")
                 {
                     break;
                 }
-                else { 
+                else
+                {
                     Console.WriteLine("Por favor introducir un valor correcto");
-                    
                 }
             } while (true);
+
             Console.WriteLine("Pulse una tecla para continuar");
             Console.ReadKey();
-            Console.Clear();            
+            Console.Clear();
         }
 
- 
+
         private static void ConsultarEstadoPedido()
         {
-            var pedido = DiccionarioPedido.SeleccionarPedido(); 
+            var pedido = DiccionarioPedido.SeleccionarPedido();
             if (pedido != null)
             {
                 Console.Clear();
                 pedido.MostrarPedidoFinal();
             }
+
             Console.WriteLine("Pulse una tecla para continuar");
             Console.ReadKey();
             Console.Clear();
         }
-        
+
         private static void ConsultarCuenta(long cuit)
         {
             Console.WriteLine("");
